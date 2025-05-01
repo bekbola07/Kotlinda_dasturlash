4ea6917c31a6ddc6e0e1ecdc0b2986c3bde07027
bekbola <azizbekbaxadirovich@gmail.com>
2025-05-01 11:02:27 +0500

    lab-9, 3-t

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,32 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val tv1:TextView = findViewById(R.id.tv1)
+        tv1.setOnClickListener{
+            tv1.setBackgroundColor(Color.rgb(255,0,0))
+        }
+        val tv2:TextView = findViewById(R.id.tv2)
+        tv2.setOnClickListener{
+            tv2.setBackgroundColor(Color.rgb(0,0,0))
+            tv2.setTextColor(Color.rgb(255,255,255))
+        }
+
+        val tv3:TextView = findViewById(R.id.tv3)
+        tv3.setOnClickListener{
+            tv3.setBackgroundColor(Color.rgb(0,255,0))
+        }
+
+        val tv4:TextView = findViewById(R.id.tv4)
+
+        tv4.setOnClickListener{
+            tv4.setBackgroundColor(Color.rgb(255,255,0))
+        }
+
+        val tv5:TextView = findViewById(R.id.tv5)
+        tv5.setOnClickListener{
+            tv5.setBackgroundColor(Color.rgb(0,0,255))
         }
     }
 }
