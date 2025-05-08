336dc69f4c8e5da9cdee4230d7632638f9694372
bekbola <azizbekbaxadirovich@gmail.com>
2025-05-08 10:03:02 +0500

    lab-10, 1-rasm

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,31 +18,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val tv1:TextView = findViewById(R.id.tv1)
-        tv1.setOnClickListener{
-            tv1.setBackgroundColor(Color.rgb(255,0,0))
-        }
-        val tv2:TextView = findViewById(R.id.tv2)
-        tv2.setOnClickListener{
-            tv2.setBackgroundColor(Color.rgb(0,0,0))
-            tv2.setTextColor(Color.rgb(255,255,255))
-        }
-
-        val tv3:TextView = findViewById(R.id.tv3)
-        tv3.setOnClickListener{
-            tv3.setBackgroundColor(Color.rgb(0,255,0))
-        }
-
-        val tv4:TextView = findViewById(R.id.tv4)
-
-        tv4.setOnClickListener{
-            tv4.setBackgroundColor(Color.rgb(255,255,0))
-        }
-
-        val tv5:TextView = findViewById(R.id.tv5)
-        tv5.setOnClickListener{
-            tv5.setBackgroundColor(Color.rgb(0,0,255))
-        }
     }
 }
