ca91a8dce84492b16644a757a5f2088fd7025805
bekbola <azizbekbaxadirovich@gmail.com>
2025-05-08 11:05:40 +0500

    lab-10, 2-rasm

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.myapplication
 
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val e1 = findViewById<EditText>(R.id.e1)
+        val t1 = findViewById<TextView>(R.id.t1)
+        e1.addTextChangedListener {
+            t1.text=e1.text
         }
     }
 }
