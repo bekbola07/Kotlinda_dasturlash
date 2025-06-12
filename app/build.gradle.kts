9086e3190449f061a5f5e890e38f499df0c56444
bekbola <azizbekbaxadirovich@gmail.com>
2025-06-12 10:20:14 +0500

    before 15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation(libs.material.v1120)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
